5342f64ac4d20d515900ceaae63639814fce42f7
Michael Holtmann <michael.holtmann456@gmail.com>
2024-02-07 09:52:59 -0500

    die class started

--- APCS PA10 Arrays of Objects TEMPLATE (23-24)/DieDriver.java
+++ APCS PA10 Arrays of Objects TEMPLATE (23-24)/DieDriver.java
@@ -6,10 +6,20 @@ public class DieDriver
 {
     public static void main(String[ ]args )
     {
-       
-        
-        
-        
+    	Die[] list = new Die[5];
+    	
+    	print(list);
+    	
+	    Die d1 = new Die();
+	    list[0] = d1;
+	    
+	    list[1].flip();
+	    print.list[1];
+	    
+	    System.out.println(d1);
+	    
+	    
+	    list[list.length - 1] = new Die;
         
         
     }
